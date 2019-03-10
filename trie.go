756da1cfffd84bc060aaf37dbefb7c668090026b
George Moschovitis <gmosx@reizu.com>
2019-03-10 20:39:21 +0200

    Minor

--- trie.go
+++ trie.go
@@ -15,29 +15,11 @@ type Trie struct {
 	children map[string]*Trie // TODO: strange to use map within a trie :-|
 }
 
-func splitter(path string, start int) (segment string, next int) {
-	// TODO: remove some overzealous checks?
-	if len(path) == 0 || start < 0 || start > len(path)-1 {
-		return "", -1
-	}
-
-	end := strings.IndexRune(path[start+1:], '/')
-	if end == -1 {
-		return path[start+1:], -1
-	}
-
-	return path[start+1 : start+end+1], start + end + 1
-}
-
 // NewTrie allocates and returns a new *Trie.
 func NewTrie() *Trie {
 	return &Trie{
 		children: make(map[string]*Trie),
 	}
-}
-
-func isParam(key string) bool {
-	return strings.HasPrefix(key, ":") // TODO: optimize me!
 }
 
 // Put inserts a new value into the tree.
@@ -65,19 +47,6 @@ func (t *Trie) Put(key string, val Value) bool {
 	return isNewVal
 }
 
-func selectChild(node *Trie, key string) *Trie {
-	c, found := node.children[key]
-	if found {
-		return c
-	}
-
-	if node.param != "" {
-		return node.children[node.param]
-	}
-
-	return nil
-}
-
 // Get returns the value associated with the given key.
 func (t *Trie) Get(key string) (Value, bool) {
 	node := t
@@ -92,4 +61,35 @@ func (t *Trie) Get(key string) (Value, bool) {
 	}
 
 	return node.value, true
+}
+
+func splitter(path string, start int) (segment string, next int) {
+	// TODO: remove some overzealous checks?
+	if len(path) == 0 || start < 0 || start > len(path)-1 {
+		return "", -1
+	}
+
+	end := strings.IndexRune(path[start+1:], '/')
+	if end == -1 {
+		return path[start+1:], -1
+	}
+
+	return path[start+1 : start+end+1], start + end + 1
+}
+
+func selectChild(node *Trie, key string) *Trie {
+	c, found := node.children[key]
+	if found {
+		return c
+	}
+
+	if node.param != "" {
+		return node.children[node.param]
+	}
+
+	return nil
+}
+
+func isParam(key string) bool {
+	return strings.HasPrefix(key, ":") // TODO: optimize me!
 }
